86ea9ae02588b9f68413b448b0d3e21b3417b553
Lopjesus123 <jesuslopezg03@outlook.com>
2025-04-21 02:38:47 -0600

    eliminacion de models.py

--- ProyectoWebApp/models.py
+++ ProyectoWebApp/models.py
@@ -1,13 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-from django.db import models
-
-class Auto(models.Model):
-    marca = models.CharField(max_length=50)
-    modelo = models.CharField(max_length=50)
-    año = models.IntegerField()
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
-    #imagen = models.ImageField(upload_to='autos/')  # Asegúrate de tener Pillow instalado
-
