a15d331a43150aeb91699b5bd5ddc8eb44b7e32f
Lopjesus123 <jesuslopezg03@outlook.com>
2025-04-22 15:00:45 -0600

    conexion entre contacto y gmail

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -125,6 +125,19 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+# conf de email
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = "agenciajm123@gmail.com"
+EMAIL_HOST_PASSWORD="ugyiolrdzipnbygd"
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- contacto/views.py
+++ contacto/views.py
@@ -1,27 +1,39 @@
 from django.shortcuts import render, redirect
-
+from django.core.mail import EmailMessage
 from .forms import FormularioContacto
-# Create your views here.
-
-
+from django.conf import settings  # 👈 Importas aquí
 
 def contacto(request):
     formulario_contacto = FormularioContacto()
-    enviado = False  # Agregamos esta línea
+    enviado = False
 
     if request.method == "POST":
         formulario_contacto = FormularioContacto(data=request.POST)
         if formulario_contacto.is_valid():
             nombre = request.POST.get("nombre")
-            email = request.POST.get("email")
+            correo = request.POST.get("email")
             contenido = request.POST.get("contenido")
 
-            return redirect("/contacto/?valido=True")
+            # 👇 Verificación en consola
+            print("💡 EMAIL HOST:", settings.EMAIL_HOST)
 
-    # Verificamos si ?valido=True está en la URL
-    if 'valido' in request.GET:
-        enviado = True
+            email = EmailMessage(
+                "Mensaje desde App Django",
+                "El usuario {} con la dirección {} escribe lo siguiente:\n\n{}".format(nombre, correo, contenido),
+                settings.EMAIL_HOST_USER,
+                ["agenciajm123@gmail.com"],  # Puedes cambiar por otro si lo necesitas
+                reply_to=[correo]
+            )
 
+            try:
+                email.send()
+                print("Correo enviado correctamente")
+                return redirect("/contacto/?valido=True")
+            except Exception as e:
+                print("ERROR AL ENVIAR CORREO:", e)
+                return redirect("/contacto/?novalido=True")
+
+    enviado = 'valido' in request.GET
     return render(request, "contacto/contacto.html", {
         'miFormulario': formulario_contacto,
         'enviado': enviado
