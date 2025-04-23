d94d0e063de3ec575c78359444e1cc02d209c962
Lopjesus123 <jesuslopezg03@outlook.com>
2025-04-22 18:29:56 -0600

    creacion de modelos de autos v2

--- autos/models.py
+++ autos/models.py
@@ -7,6 +7,12 @@ from django.db import models
 
 class Marca(models.Model):
     nombre = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Marca"
+        verbose_name_plural = "Marcas"
 
     def __str__(self):
         return self.nombre
@@ -21,6 +27,13 @@ class Auto(models.Model):
     imagen = models.ImageField(upload_to='autos/')
     disponible = models.BooleanField(default=True)
     stock = models.PositiveIntegerField(default=1)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        verbose_name = "Auto"
+        verbose_name_plural = "Autos"
 
     def __str__(self):
         return f"{self.marca} {self.modelo} ({self.año})"
